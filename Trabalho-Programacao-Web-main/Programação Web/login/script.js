da07b81e436481a5353d276092fb6fdb18c29777
Felipe Henrique <93741479+EoDreamy@users.noreply.github.com>
2025-05-29 20:07:55 -0300

    Add files via upload

---
+++ Trabalho-Programacao-Web-main/Programação Web/login/script.js
@@ -0,0 +1,17 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    // Impede o envio padrão do formulário (evita recarregar a página)
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMsg = document.getElementById("error");
+    // Pega os valores digitados nos campos e a área para mostrar erro
+
+    if (username === "admin" && password === "123") {
+        window.location.href ="../home/index.html"; 
+        // Se login estiver correto, redireciona para a página principal
+    } else {
+        errorMsg.textContent = "Usuário ou senha inválidos.";
+        // Se estiver errado, exibe mensagem de erro na tela
+    }
+});
